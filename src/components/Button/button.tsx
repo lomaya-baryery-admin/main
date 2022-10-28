6d87493568fea39b5041dbff317da1d7c9ad9258
Aleksandr Dronov <dronovspb@gmail.com>
2022-10-28 19:19:36 +0600

    add_component_alert

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -7,30 +7,28 @@ interface ButtonProps {
   size?: 'small' | 'large';
   label?: string;
   onClick?: () => void;
-}  
+}
 
 export const Button: FC<ButtonProps> = ({
-  type = 'main', 
+  type = 'main',
   primary = true,
   size = 'large',
   label,
   ...props
 }) => {
   let mode;
-  if(type === 'main') {
+  if (type === 'main') {
     mode = primary ? `${styles.main_primary}` : `${styles.main_secondary}`;
   } else if (type === 'not_main') {
     mode = primary ? `${styles.not_main_primary}` : `${styles.not_main_secondary}`;
   } else {
-    mode = primary ? `${styles.not_main_negative_primary}` : `${styles.not_main_negative_secondary}`;
+    mode = primary
+      ? `${styles.not_main_negative_primary}`
+      : `${styles.not_main_negative_secondary}`;
   }
 
   return (
-    <button
-      type="button"
-      className={`${styles.button} ${styles[size]} ${mode}`}
-      {...props}
-    >
+    <button type="button" className={`${styles.button} ${styles[size]} ${mode}`} {...props}>
       {label}
     </button>
   );
