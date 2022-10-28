a7358e878055f60dac35a00a0b2ff1660d527256
kapitan4ever <stklmn@yandex.ru>
2022-10-28 16:53:06 +0300

    settings eslint/prettier

--- src/stories/icons/search-icon.tsx
+++ src/stories/icons/search-icon.tsx
@@ -6,9 +6,9 @@ export const SearchIcon = ({ type }: IIconProps) => {
       <path
         d="M18 18L15.1046 15.1046M15.1046 15.1046C16.0697 14.1394 16.6667 12.8061 16.6667 11.3333C16.6667 8.38781 14.2789 6 11.3333 6C8.38781 6 6 8.38781 6 11.3333C6 14.2789 8.38781 16.6667 11.3333 16.6667C12.8061 16.6667 14.1394 16.0697 15.1046 15.1046Z"
         stroke={getColor(type)}
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
