8ce33007ba509f3c82f6b51e1882e91727972292
lizonkisel <ees.hwork@gmail.com>
2022-10-31 12:16:00 +0300

    feat: add blanks for applications pages

---
+++ src/pages/reviewed-applications-page/reviewed-applications-page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './reviewed-applications-page.module.css';
+
+import { Alert } from '../../stories/alert/alert';
+
+export function ReviewedApplicationsPage() {
+    return (
+        <main className={styles.main}>
+            <section className={styles.header_content}>
+                <h1 className={styles.header}>Заявки на участие</h1>
+            </section>
+            <section className={styles.main_content}>
+                <Alert title='Завершённые заявки на участие отсутствуют'/>
+            </section>
+            <section className={styles.footer_content}></section>
+        </main>
+    )
+}
