05b09983232f7afc056e35b4f02f4a842eb4d6eb
Marina Kravchenko <marishka-k87@yandex.ru>
2022-10-31 22:53:51 +0300

    add pagination-button component

---
+++ src/stories/paginations/paginations-button/paginations-button.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './paginations-button.module.css';
+
+interface IPaginationsButtonProps {
+  buttonName: number | string;
+  buttonActive: 'active' | 'inactive' | 'disabled';
+  textActive: 'active' | 'inactive';
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export const PaginationsButton = ({
+  buttonName,
+  onClick,
+  buttonActive = 'active',
+  textActive = 'active',
+}: IPaginationsButtonProps) => {
+  const buttonActiveClass =
+    buttonActive === 'active'
+      ? `${styles.button_active}`
+      : buttonActive === 'inactive'
+      ? `${styles.button_inactive}`
+      : `${styles.button_disabled}`;
+
+  const textActiveClass =
+    textActive === 'active' ? `${styles.text_active}` : `${styles.text_inactive}`;
+
+  const currentClasses = [`${styles.button}`, `${buttonActiveClass}`, `${textActiveClass}`].join(
+    ' '
+  );
+
+  return (
+    <button className={currentClasses} onClick={onClick}>
+      {buttonName}
+    </button>
+  );
+};
