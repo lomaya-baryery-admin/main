f015f3e92f291f245c360e00e0d651355b092ad1
Nikolay Smolov <n.smolov@yandex.ru>
2022-10-31 10:02:34 +0300

    adds spreadsheet links common styles

--- src/stories/link.stories.tsx
+++ src/stories/link.stories.tsx
@@ -1,41 +1,9 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { Link } from './link';
-import './link.css';
+import type { Meta } from '@storybook/react';
 
 export default {
-  title: 'link',
-  Comment: Link,
-  argTypes: {
-    linkName: {
-      type: 'string',
-      defaultValue: 'Имя Фамилия',
-      description: 'имя ссылки',
-    },
-    size: {
-      type: 'string',
-      description: 'ширина (max-content, min-content), размер шрифта и отступы',
-      defaultValue: 'default',
-      options: ['default', 'small'],
-      control: {
-        type: 'radio',
-      },
-    },
-    onClick: {
-      type: 'function',
-      description: 'принимает значением функцию',
-    },
-  },
-} as ComponentMeta<typeof Link>;
+  title: 'SpreadsheetLink',
+} as Meta;
 
-const Template: ComponentStory<typeof Link> = (args) => <Link {...args} />;
-
-export const Default = Template.bind({});
-Default.args = {
-  linkName: 'Имя Фамилия',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  linkName: 'Имя Фамилия',
-  size: 'small',
+export const Default = (args: unknown) => {
+  return <a className={'text text_type_main-default spreadsheetLink'}>Название задания</a>;
 };
