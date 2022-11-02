c65b3e9a7a30c11d461c7b2d9136e8dffbbbda83
vio-ubuntu <saiviolet@gmail.com>
2022-11-02 13:41:17 +0300

    Вернула дефолтный App

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import React, { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 import { useGetDefaultQuery, useShiftsPostMutation } from './redux-store/api-slice/api-slice'
-import { Table } from './stories/table/Table';
-import { allReportsColumns, AllReportsData, AllShiftData, shiftColumns } from './utils/tableColumns';
-import { ImagePreview } from './stories/image-preview/ImagePreview';
 
 fetch('http://51.250.32.125:8000/hello')
   .then(res => res.json())
@@ -27,33 +24,27 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    // <div className="App">
-    //   <div>
-    //     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-    //       <img src="/vite.svg" className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-    //       {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button type="button" onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <button type="button" onClick={handleShiftPost}>postShifts</button>
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    // </div>
-    <>
-
-      <Table defaultData={AllReportsData} columnsData={allReportsColumns} rowHeight={80}/>
-
-    </>
-
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button type="button" onClick={handleShiftPost}>postShifts</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </div>
   );
 }
 
