41cb92310ecbdcaf505a94d1b165b0ad18c3adf7
Alena Ismagilova <ismagilovaalena90@yandex.ru>
2022-11-03 23:24:25 +0300

    add search-input in storybook

---
+++ src/stories/search-input/search-input.stories.tsx
@@ -0,0 +1,30 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import { SearchInput } from './search-input';
+
+export default {
+  title: 'SearchInput',
+  component: SearchInput,
+  argTypes: {
+    value: {
+      description: 'Значение поля',
+      type: 'string',
+      defaultValue: '',
+    },
+    onChange: {
+      description: 'Optional change handler',
+      type: 'function',
+    },
+    onClear: {
+      description: 'Clear input',
+      type: 'function',
+    },
+    onFocus: {
+      description: 'Focus input',
+      type: 'function',
+    },
+  },
+} as ComponentMeta<typeof SearchInput>;
+
+const Template: ComponentStory<typeof SearchInput> = (args) => <SearchInput {...args} />;
+
+export const Default = Template.bind({});
---
+++ src/stories/search-input/search-input.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import { CloseIcon } from '../icons';
+import { Search } from '../icons/search-icon.stories';
+import styles from './search-input.module.css';
+
+interface ISearchProps {
+  value: string;
+  onChange: (e: React.SyntheticEvent<EventTarget>) => void;
+  onClear: () => void;
+  onFocus: () => void;
+}
+
+export const SearchInput = ({ value, onChange, onClear, onFocus }: ISearchProps) => {
+  const [iconClose, setIconClose] = useState(false);
+
+  const onMouseEnterHandler = () => {
+    setIconClose(true);
+  };
+
+  const onMouseLeaveHandler = () => {
+    setIconClose(false);
+  };
+  return (
+    <div>
+      <div className={styles.form}>
+        <form className={styles.search__form}>
+          <label htmlFor="name" className={styles.search__label}>
+            <Search type="interface-secondary" onClick={onFocus} />
+            <input
+              type="text"
+              value={value}
+              placeholder="Поиск"
+              className={styles.search__input}
+              name="name"
+              onChange={onChange}
+            />
+            {value ? (
+              <div
+                className={styles.search__closeIconWrapper}
+                onMouseEnter={onMouseEnterHandler}
+                onMouseLeave={onMouseLeaveHandler}
+              >
+                {iconClose ? (
+                  <CloseIcon
+                    type="interface-black"
+                    className={styles.search__closeIcon}
+                    onClick={onClear}
+                  />
+                ) : (
+                  <CloseIcon
+                    type="interface-secondary"
+                    className={styles.search__closeIcon}
+                    onClick={onClear}
+                  />
+                )}
+              </div>
+            ) : null}
+          </label>
+        </form>
+      </div>
+    </div>
+  );
+};
