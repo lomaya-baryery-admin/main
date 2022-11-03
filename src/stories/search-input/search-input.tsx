0cb5e95bbeaca5ed7df93e693e1d0c0cbebda136
Nikolay Smolov <n.smolov@yandex.ru>
2022-11-04 02:51:28 +0300

    refactor common/input-search

--- src/stories/search-input/search-input.tsx
+++ src/stories/search-input/search-input.tsx
@@ -1,63 +1,45 @@
-import { useState } from 'react';
-import { CloseIcon } from '../../ui/icons/close-icon';
-import { Search } from '../icons/search-icon.stories';
+import React, { useRef, useState } from 'react';
+import { CloseIcon, SearchIcon } from '../../ui/icons';
 import styles from './search-input.module.css';
 
 interface ISearchProps {
   value: string;
-  onChange: (e: React.SyntheticEvent<EventTarget>) => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onClear: () => void;
-  onFocus: () => void;
 }
 
-export const SearchInput = ({ value, onChange, onClear, onFocus }: ISearchProps) => {
-  const [iconClose, setIconClose] = useState(false);
+export const SearchInput = ({ value, onChange, onClear }: ISearchProps) => {
+  const [onField, setState] = useState(false);
 
-  const onMouseEnterHandler = () => {
-    setIconClose(true);
-  };
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  const onMouseLeaveHandler = () => {
-    setIconClose(false);
-  };
   return (
-    <div>
-      <div className={styles.form}>
-        <form className={styles.search__form}>
-          <label htmlFor="name" className={styles.search__label}>
-            <Search type="interface-secondary" onClick={onFocus} />
-            <input
-              type="text"
-              value={value}
-              placeholder="Поиск"
-              className={styles.search__input}
-              name="name"
-              onChange={onChange}
-            />
-            {value ? (
-              <div
-                className={styles.search__closeIconWrapper}
-                onMouseEnter={onMouseEnterHandler}
-                onMouseLeave={onMouseLeaveHandler}
-              >
-                {iconClose ? (
-                  <CloseIcon
-                    type="interface-black"
-                    className={styles.search__closeIcon}
-                    onClick={onClear}
-                  />
-                ) : (
-                  <CloseIcon
-                    type="interface-secondary"
-                    className={styles.search__closeIcon}
-                    onClick={onClear}
-                  />
-                )}
-              </div>
-            ) : null}
-          </label>
-        </form>
-      </div>
-    </div>
+    <label
+      htmlFor="name"
+      className={`${styles.searchField} ${value || onField ? styles.searchField_interact : null}`}
+    >
+      <SearchIcon
+        type="interface-secondary"
+        className={styles.search__searchIcon}
+        onClick={() => {
+          inputRef.current?.focus();
+        }}
+      />
+      <input
+        ref={inputRef}
+        type="text"
+        value={value}
+        placeholder="Поиск"
+        className={styles.search__input}
+        name="name"
+        onChange={onChange}
+        autoComplete={'off'}
+        onFocus={() => setState(true)}
+        onBlur={() => setState(false)}
+      />
+      {value ? (
+        <CloseIcon type="interface-black" className={styles.search__closeIcon} onClick={onClear} />
+      ) : null}
+    </label>
   );
 };
