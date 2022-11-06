8d0d8acdbcaddd63874961cea65c081c70a2a3c6
Marina Kravchenko <marishka-k87@yandex.ru>
2022-11-06 15:41:56 +0300

    fix prettier

--- src/stories/paginations/paginations.tsx
+++ src/stories/paginations/paginations.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { PaginationsButton } from './paginations-button/paginations-button';
+
 import styles from './paginations.module.css';
 interface IPaginationsProps {
   counterPages: number;
@@ -10,74 +11,92 @@ interface IPaginationsProps {
 export const Paginations = ({ counterPages, currentPage, setCurrentPage }: IPaginationsProps) => {
   const arrPages = Array.from({ length: counterPages }, (v, i) => i + 1);
 
+  const dots = '...';
 
-  const dots = '...' 
+  const [arrForButtonsPanel, setArrForButtonsPanel] = useState<Array<number | '...'>>([]);
 
-  const [arrForButtonsPanel, setArrForButtonsPanel] = useState<Array<number | '...'>>([])
+  useEffect(() => {
+    let tempNumberOfPages: Array<number | '...'> = [...arrPages];
 
-  useEffect (() => {
-    let tempNumberOfPages: Array<number | "..."> = [...arrPages]
-   
     if (currentPage >= 1 && currentPage <= 3) {
-      tempNumberOfPages = [1, 2, 3, 4, dots, counterPages]
+      tempNumberOfPages = [1, 2, 3, 4, dots, counterPages];
+    } else if (currentPage === 4) {
+      const sliced = arrPages.slice(0, 5);
+      tempNumberOfPages = [...sliced, dots, counterPages];
+    } else if (currentPage > 4 && currentPage < counterPages - 2) {
+      const sliced1 = arrPages.slice(currentPage - 2, currentPage);
+      const sliced2 = arrPages.slice(currentPage, currentPage + 1);
+      tempNumberOfPages = [1, dots, ...sliced1, ...sliced2, dots, counterPages];
+    } else if (currentPage > counterPages - 3) {
+      const sliced = arrPages.slice(counterPages - 4);
+      tempNumberOfPages = [1, dots, ...sliced];
     }
 
-    else if (currentPage === 4 ) {
-      const sliced = arrPages.slice(0, 5)
-      tempNumberOfPages = [...sliced, dots, counterPages]
-    }
-
-    else if (currentPage > 4 && currentPage < counterPages - 2 ) {
-      const sliced1 = arrPages.slice(currentPage - 2, currentPage )
-      const sliced2 = arrPages.slice(currentPage, currentPage + 1 )
-      tempNumberOfPages = [1, dots, ...sliced1, ...sliced2, dots, counterPages]
-    }
-
-    else if (currentPage > counterPages - 3 ) {
-      const sliced = arrPages.slice(counterPages - 4)
-      tempNumberOfPages = [1, dots, ...sliced]
-    }   
-
-    setArrForButtonsPanel(tempNumberOfPages)
+    setArrForButtonsPanel(tempNumberOfPages);
     console.log(arrForButtonsPanel);
-    
-  }, [currentPage, counterPages])
+  }, [currentPage, counterPages]);
 
   return (
     <div className={styles.content}>
-      {currentPage > 1 ? 
+      {currentPage > 1 ? (
         <>
-          <PaginationsButton buttonName="<<" onClick={() => setCurrentPage(1)} buttonActive="inactive"/>
-          <PaginationsButton buttonName="<" onClick={() => setCurrentPage((prev:number) => prev === 1 ? prev : prev - 1)} buttonActive="inactive"/>
+          <PaginationsButton
+            buttonName="<<"
+            onClick={() => setCurrentPage(1)}
+            buttonActive="inactive"
+          />
+          <PaginationsButton
+            buttonName="<"
+            onClick={() => setCurrentPage((prev: number) => (prev === 1 ? prev : prev - 1))}
+            buttonActive="inactive"
+          />
         </>
-      :
+      ) : (
         <>
           <PaginationsButton buttonName="<" buttonActive="disabled" textActive="inactive" />
           <PaginationsButton buttonName="<<" buttonActive="disabled" textActive="inactive" />
         </>
-      }
-
-      {arrForButtonsPanel.map((page, index) =>
-        page === dots 
-        ? 
-        <PaginationsButton key={index} buttonName={page} buttonActive="disabled" textActive="inactive" />
-        : 
-        <PaginationsButton key={index} buttonName={page} buttonActive={page !== currentPage ? "disabled" : "active"} textActive={page !== currentPage ? "inactive" : "active"}/>              
       )}
 
-      {currentPage < counterPages ? 
+      {arrForButtonsPanel.map((page, index) =>
+        page === dots ? (
+          <PaginationsButton
+            key={index}
+            buttonName={page}
+            buttonActive="disabled"
+            textActive="inactive"
+          />
+        ) : (
+          <PaginationsButton
+            key={index}
+            buttonName={page}
+            buttonActive={page !== currentPage ? 'disabled' : 'active'}
+            textActive={page !== currentPage ? 'inactive' : 'active'}
+          />
+        )
+      )}
+
+      {currentPage < counterPages ? (
         <>
-          <PaginationsButton buttonName=">" onClick={() => setCurrentPage((prev:number) => prev === counterPages? prev : prev + 1)} buttonActive="inactive"/>
-          <PaginationsButton buttonName=">>" onClick={() => setCurrentPage(counterPages)} buttonActive="inactive"/>
+          <PaginationsButton
+            buttonName=">"
+            onClick={() =>
+              setCurrentPage((prev: number) => (prev === counterPages ? prev : prev + 1))
+            }
+            buttonActive="inactive"
+          />
+          <PaginationsButton
+            buttonName=">>"
+            onClick={() => setCurrentPage(counterPages)}
+            buttonActive="inactive"
+          />
         </>
-       : 
+      ) : (
         <>
           <PaginationsButton buttonName=">" buttonActive="disabled" textActive="inactive" />
           <PaginationsButton buttonName=">>" buttonActive="disabled" textActive="inactive" />
         </>
-      }
+      )}
     </div>
   );
 };
-
-
