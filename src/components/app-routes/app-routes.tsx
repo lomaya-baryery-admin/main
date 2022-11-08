6190e636441535bc2e2ae414dc1d3e679a514008
Nikolay Smolov <n.smolov@yandex.ru>
2022-11-08 12:54:46 +0300

    fix storybook, root render switched on react v18

--- src/components/app-routes/app-routes.tsx
+++ src/components/app-routes/app-routes.tsx
@@ -1,43 +1,21 @@
-import { ChangeEvent, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { SearchInput } from '../../ui/search-Input/search-input';
 import { Layout } from '../layout/layout';
 
-export const AppRoutes = () => {
-  const [value, setValue] = useState('');
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.currentTarget.value);
-  };
-
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route
-            path="/shift/all"
-            element={
-              <div>
-                {' '}
-                <SearchInput
-                  onChange={handleChange}
-                  onClear={() => setValue('')}
-                  value={value}
-                />{' '}
-              </div>
-            }
-          />
-          <Route path="/shift/current" element={<div>2</div>} />
-          <Route path="/shift/new" element={<div>3</div>} />
-          <Route path="/invites/active" element={<div>4</div>} />
-          <Route path="/invites/reviewed" element={<div>5</div>} />
-          <Route path="/participants" element={<div>6</div>} />
-          <Route path="/report/noverified" element={<div>7</div>} />
-          <Route path="/report/verified" element={<div>8</div>} />
-          <Route path="/report/rejected" element={<div>9</div>} />
-          <Route path="*" element={<div>страница не найдена </div>} />
-        </Route>
-      </Routes>
-    </div>
-  );
-};
+export const AppRoutes = () => (
+  <div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/shift/all" element={<div>1</div>} />
+        <Route path="/shift/current" element={<div>2</div>} />
+        <Route path="/shift/new" element={<div>3</div>} />
+        <Route path="/invites/active" element={<div>4</div>} />
+        <Route path="/invites/reviewed" element={<div>5</div>} />
+        <Route path="/participants" element={<div>6</div>} />
+        <Route path="/report/noverified" element={<div>7</div>} />
+        <Route path="/report/verified" element={<div>8</div>} />
+        <Route path="/report/rejected" element={<div>9</div>} />
+        <Route path="*" element={<div>страница не найдена </div>} />
+      </Route>
+    </Routes>
+  </div>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux-store/store';
 import App from './components/app/app';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
