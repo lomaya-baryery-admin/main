11868f583d0241138e90993a284acbbd056e02b3
Nikolay Smolov <n.smolov@yandex.ru>
2022-11-08 12:15:04 +0300

    switch nanoid on redux/toolkit, adds classnames lib, add blur interaction with value

--- src/ui/search-Input/search-input.tsx
+++ src/ui/search-Input/search-input.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { nanoid } from 'nanoid';
+import { nanoid } from '@reduxjs/toolkit';
+import cn from 'classnames';
 import { CloseIcon, SearchIcon } from '../icons';
 import styles from './search-input.module.css';
 
@@ -7,23 +8,31 @@ interface ISearchProps {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onClear: () => void;
+  className?: string;
 }
 
-export const SearchInput = ({ value, onChange, onClear }: ISearchProps) => (
-  <label htmlFor="name" className={styles.searchField}>
-    <SearchIcon type="interface-secondary" className={styles.search__searchIcon} />
-    <input
-      id={`${nanoid()}`}
-      type="text"
-      value={value}
-      placeholder="Поиск"
-      className={styles.search__input}
-      name="name"
-      onChange={onChange}
-      autoComplete="off"
-    />
-    {value ? (
-      <CloseIcon type="interface-black" className={styles.search__closeIcon} onClick={onClear} />
-    ) : null}
-  </label>
-);
+export const SearchInput = ({ value, onChange, onClear, className }: ISearchProps) => {
+  const inputId = nanoid();
+
+  return (
+    <label
+      htmlFor={inputId}
+      className={cn(styles.searchField, { [styles.searchField_active]: value })}
+    >
+      <SearchIcon type="interface-secondary" className={styles.search__searchIcon} />
+      <input
+        id={inputId}
+        type="text"
+        value={value}
+        placeholder="Поиск"
+        className={cn(styles.search__input, { [styles.search__input_active]: value })}
+        name="name"
+        onChange={onChange}
+        autoComplete="off"
+      />
+      {value ? (
+        <CloseIcon type="interface-black" className={styles.search__closeIcon} onClick={onClear} />
+      ) : null}
+    </label>
+  );
+};
