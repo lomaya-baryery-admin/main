c0576b4b18a5d9be600e5474c8e7c6c42589b353
kapitan4ever <stklmn@yandex.ru>
2022-11-09 22:37:50 +0300

    add styles input search

--- src/pages/reportsPage/ReportsPage.tsx
+++ src/pages/reportsPage/ReportsPage.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import Search from '../../components/search/search';
+import { SearchInput } from '../../ui/search-Input/search-input';
 import { Table } from '../../ui/table/Table';
 import styles from './reports.module.css';
 import { allReportsColumns, AllReportsData } from '../../utils/tableColumns';
@@ -8,7 +8,7 @@ const ReportsPage: FC = () => (
   <section className={styles.container}>
     <div className={styles.container__header}>
       <h2 className={styles.container__title}>Отчеты участников</h2>
-      <Search />
+      <SearchInput value="" onChange={() => {}} onClear={() => {}} />
     </div>
     <Table columnsData={allReportsColumns} defaultData={AllReportsData} rowHeight={80} />
   </section>
