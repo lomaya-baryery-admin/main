ef7a9b658758d2842d8acc398a2d264b9982bf38
Marina Kravchenko <marishka-k87@yandex.ru>
2022-11-09 18:35:33 +0300

    Update paginations.tsx

--- src/stories/paginations/paginations.tsx
+++ src/stories/paginations/paginations.tsx
@@ -29,7 +29,9 @@ export const Paginations = ({ counterPages, currentPage, setCurrentPage }: IPagi
   useEffect(() => {
     let tempNumberOfPages: Array<number | '...'> = [...arrPages];
 
-    if (currentPage >= 1 && currentPage <= 3) {
+    if (counterPages <= 5) {
+      tempNumberOfPages = arrPages
+    } else if (currentPage >= 1 && currentPage <= 3) {
       tempNumberOfPages = [1, 2, 3, 4, dots, counterPages];
     } else if (currentPage === 4) {
       const sliced = arrPages.slice(0, 5);
@@ -67,6 +69,8 @@ export const Paginations = ({ counterPages, currentPage, setCurrentPage }: IPagi
           <PaginationsButton buttonName="<<" buttonActive="disabled" textActive="inactive" />
         </>
       )}
+
+
 
       {arrForButtonsPanel.map((page) =>
         page === dots ? (
