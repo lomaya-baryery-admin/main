131c5fa0cba75e02a8bf201cfe8da27e558d2e86
kapitan4ever <stklmn@yandex.ru>
2022-11-12 11:15:24 +0300

    update page

--- src/components/app-routes/app-routes.tsx
+++ src/components/app-routes/app-routes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from '../layout/layout';
 import ReportsPage from '../../pages/reportsPage/ReportsPage';
+import ParticipantReport from '../../pages/participantReport/participantReport';
 
 export const AppRoutes = () => (
   <div>
@@ -13,7 +14,7 @@ export const AppRoutes = () => (
         <Route path="/invites/reviewed" element={<div>5</div>} />
         <Route path="/participants" element={<div>6</div>} />
         <Route path="/report/noverified" element={<ReportsPage />} />
-        <Route path="/report/verified" element={<div>8</div>} />
+        <Route path="/report/verified" element={<ParticipantReport />} />
         <Route path="/report/rejected" element={<div>9</div>} />
         <Route path="*" element={<div>страница не найдена </div>} />
       </Route>
---
+++ src/pages/participantReport/participantReport.tsx
@@ -0,0 +1,45 @@
+import React, {FC} from 'react';
+import participantReportStyle from './participantReport.module.css';
+import imageDefault from "../../ui/preview/image5.jpg";
+import imageSmallDefault from "./images/imgSmall.jpg"
+import {Button} from "../../ui/button/button";
+import {ArrowLeftIcon, ArrowRightIcon, CheckIcon, CloseIcon} from "../../ui/icons";
+
+const ParticipantReport: FC = () => {
+    return (
+        <section className={participantReportStyle.container}>
+            <p className={participantReportStyle.touch}>Отчёты участников / Название задания</p>
+            <h2 className={participantReportStyle.header}>Название задания</h2>
+            <h3 className={participantReportStyle.player}>Участник</h3>
+            <p className={participantReportStyle.send}>Отправлено</p>
+            <p className={participantReportStyle.name}>Ivanov Ivanov</p>
+            <p className={participantReportStyle.time}> 00.00.00 b 00.00 </p>
+            <Button htmlType={'button'} type="secondary" size={'small'} className={participantReportStyle.buttonHistory}>
+                История отправки
+            </Button>
+            <Button htmlType={'button'} size={'large'} className={participantReportStyle.buttonAccept}>
+                <CheckIcon type="interface-white" />
+                Одобрить
+            </Button>
+            <Button htmlType={'button'} size={'large'} className={participantReportStyle.buttonCancel}>
+                <CloseIcon type="interface-white" />
+                Отклонить
+            </Button>
+            <div className={participantReportStyle.image}>
+                <img alt={imageSmallDefault} src={imageSmallDefault} className={participantReportStyle.smallImg} />
+                <img alt={imageDefault} src={imageDefault} className={participantReportStyle.pictureImg}/>
+            </div>
+            <Button htmlType={'button'} type="secondary" size={'small'} className={participantReportStyle.buttonLeft}>
+                <ArrowLeftIcon type="link-active"/>
+                Предыдущее задание
+            </Button>
+            <Button htmlType={'button'} type="secondary" size={'small'} className={participantReportStyle.buttonRight}>
+                Следующее задание
+                <ArrowRightIcon type="link-active"/>
+            </Button>
+
+        </section>
+    )
+}
+
+export default ParticipantReport;
