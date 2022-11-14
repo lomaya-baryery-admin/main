01a0f9a6b175edc7de443259b3c8b1f5368c4e5e
lizonkisel <ees.hwork@gmail.com>
2022-11-14 17:07:17 +0300

    feat: add markdowm file with docs for applications pages

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,17 +3,12 @@ import { Header } from '../header/header';
 import { Navbar } from '../navbar/navbar';
 import styles from './layout.module.css';
 
-import { ActiveApplicationsPage } from '../../pages/active-applications-page/active-applications-page';
-import { ReviewedApplicationsPage } from '../../pages/reviewed-applications-page/reviewed-applications-page';
-
 export const Layout = () => (
   <div>
     <Header />
     <main className={styles.main}>
       <Navbar />
       <Outlet />
-      <ActiveApplicationsPage />
-      {/* <ReviewedApplicationsPage /> */}
     </main>
   </div>
 );
