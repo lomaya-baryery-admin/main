650917491f6abeed1a9d928184ca5b6d37c87530
Victor Stepanov <vix522430@bk.ru>
2022-11-14 19:41:57 +0300

    feat:added rtk-q

--- src/components/app-routes/app-routes.tsx
+++ src/components/app-routes/app-routes.tsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
+import { ActiveApplicationsPage } from '../../pages/active-applications-page/active-applications-page';
+import { ReviewedApplicationsPage } from '../../pages/reviewed-applications-page/reviewed-applications-page';
 import { Layout } from '../layout/layout';
 
 export const AppRoutes = () => (
@@ -8,8 +10,8 @@ export const AppRoutes = () => (
         <Route path="/shift/all" element={<div>1</div>} />
         <Route path="/shift/current" element={<div>2</div>} />
         <Route path="/shift/new" element={<div>3</div>} />
-        <Route path="/invites/active" element={<div>4</div>} />
-        <Route path="/invites/reviewed" element={<div>5</div>} />
+        <Route path="/invites/active" element={<ActiveApplicationsPage />} />
+        <Route path="/invites/reviewed" element={<ReviewedApplicationsPage />} />
         <Route path="/participants" element={<div>6</div>} />
         <Route path="/report/noverified" element={<div>7</div>} />
         <Route path="/report/verified" element={<div>8</div>} />
--- src/pages/reviewed-applications-page/reviewed-applications-page.tsx
+++ src/pages/reviewed-applications-page/reviewed-applications-page.tsx
@@ -1,62 +1,14 @@
-import React from 'react';
 import { createColumnHelper } from '@tanstack/react-table';
 
 import styles from './reviewed-applications-page.module.css';
 
 import { applicationLabel } from '../../utils/constants';
 
-import { Table } from '../../ui/table/Table';
-import { StatusLabel } from '../../ui/status-label/status-label';
+import { useFetchApplicationsQuery } from '../../redux-store/api-slice/api-slice';
 import { SearchInput } from '../../ui/search-Input/search-input';
-
-// Структура данных, приходящих с сервера
-interface requestsColumnsNames {
-  user_id: string;
-  name: string;
-  surname: string;
-  date_of_birth: string;
-  city: string;
-  phone: string;
-  request_id: string;
-  status: string;
-}
-
-// Захардкоженный массив данных, приходящих с сервера.
-// Он нужен просто для образца, в код таблицы идут преобразованные данные с бэка. Про них - ниже
-const defaultData: requestsColumnsNames[] = [
-  {
-    user_id: 'dkfkd943904004fdjfd3438',
-    name: 'Иван',
-    surname: 'Павлов',
-    date_of_birth: '26.09.1849',
-    city: 'Ленинград',
-    phone: '+79998887766',
-    request_id: 'dkfkd943904004fdjfd3438',
-    status: 'pending',
-  },
-  {
-    user_id: 'dkfkd943904004fdjfd3439',
-    name: 'Дмитрий',
-    surname: 'Менделеев',
-    date_of_birth: '08.02.1834',
-    city: 'Санкт-Петербург',
-    phone: '+79998887755',
-    request_id: 'dkfkd943904004fdjfd3439',
-    status: 'declined',
-  },
-  {
-    user_id: 'dkfkd943904004fdjfd3440',
-    name: 'Софья',
-    surname: 'Ковалевская',
-    date_of_birth: '15.01.1850',
-    city: 'Москва',
-    phone: '+79998887744',
-    request_id: 'dkfkd943904004fdjfd3440',
-    status: 'approved',
-  },
-];
-
-const checkedData = defaultData.filter(user => user.status !== 'pending');
+import { StatusLabel } from '../../ui/status-label/status-label';
+import { Table } from '../../ui/table/Table';
+import { tmpData } from './tmp';
 
 // TApplicationsTable, applicationsData, applicationsColumnsHelper, applicationsColumns
 // Это переменные, отвечающие за работу таблицы с нашими данными. По идее, такое должны написать коллеги,
@@ -71,26 +23,28 @@ type TApplicationsTable = {
   status: string;
 };
 
-const dataForTable: TApplicationsTable[] = [];
-
-// Функция, которая преобразует данные бэкенда в данные для таблицы
-function makeDataForTable() {
-  checkedData.forEach((user) => {
-    const userForTable = {
-      user_name: `${user.name} ${user.surname}`,
-      city: user.city,
-      phone: user.phone,
-      date_of_birth: user.date_of_birth,
-      status: user.status,
-    };
-    dataForTable.push(userForTable);
-  });
-}
-
-makeDataForTable();
-
-
 export function ReviewedApplicationsPage() {
+  const { data: responceData, isSuccess } = useFetchApplicationsQuery(''); //
+
+  const checkedData = isSuccess ? responceData : tmpData;
+
+  const dataForTable: TApplicationsTable[] = [];
+
+  // Функция, которая преобразует данные бэкенда в данные для таблицы
+  function makeDataForTable() {
+    checkedData.forEach((user) => {
+      const userForTable = {
+        user_name: `${user.name} ${user.surname}`,
+        city: user.city,
+        phone: user.phone,
+        date_of_birth: user.date_of_birth,
+        status: user.status,
+      };
+      dataForTable.push(userForTable);
+    });
+  }
+
+  makeDataForTable();
 
   // Вспомогательная функция из либы @tanstack/react-table (by lizonkisel)
   const applicationsColumnsHelper = createColumnHelper<TApplicationsTable>();
@@ -119,7 +73,7 @@ export function ReviewedApplicationsPage() {
     }),
     applicationsColumnsHelper.accessor((row) => row.status, {
       header: ' ',
-      // Внутри cell код, вероятнее всего, нужно будет переписывать после подключения store. 
+      // Внутри cell код, вероятнее всего, нужно будет переписывать после подключения store.
       // Для этого потребуются эксперименты с готовым store-ом
       cell: (status) => {
         const value: 'approved' | 'declined' = status.getValue();
@@ -128,10 +82,10 @@ export function ReviewedApplicationsPage() {
           typeValue = 'rejected';
         } else {
           typeValue = value;
-        };
+        }
         const icon = value === 'approved' ? 'CircleCheckIcon' : 'CircleStopIcon';
         return <StatusLabel statusText={applicationLabel(value)} type={typeValue} icon={icon} />;
-      }
+      },
     }),
   ];
 
@@ -139,10 +93,15 @@ export function ReviewedApplicationsPage() {
     <article className={styles.main}>
       <section className={styles.header_content}>
         <h1 className="text_type_main-extra-large">Заявки на участие</h1>
-        <SearchInput value='' onChange={() => {}} onClear={() => {}} />
+        <SearchInput value="" onChange={() => {}} onClear={() => {}} />
       </section>
       <section className={styles.main_content}>
-        <Table defaultData={dataForTable} columnsData={applicationsColumns} rowHeight={60} getRowCanExpand={() => false} />
+        <Table
+          defaultData={dataForTable}
+          columnsData={applicationsColumns}
+          rowHeight={60}
+          getRowCanExpand={() => false}
+        />
       </section>
       <section className={styles.footer_content}>Footer</section>
     </article>
